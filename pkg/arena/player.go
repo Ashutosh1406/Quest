f4195d02cbf2369d6429344d0de0c8fa025b43a3
Ashutosh Mishra <mishratushar388@gmail.com>
2024-07-09 03:11:33 +0530

    Player Methods Implemented Successfully

--- pkg/arena/player.go
+++ pkg/arena/player.go
@@ -1,7 +1,13 @@
 package arena
 
-type Player interface { //using go interfaces for extensibility in future...
+type Player interface { //using go interfaces for method extensibility in future...
 	//some methods to be called according to tghe functionality
+
+	GetHealth() int //for health check {rest all are self explanatory methods}
+	GetStrength() int
+	GetAttack() int
+	ReduceHealth(amount int) // amount is to be calculated according to the problem statement as contains important Game Logic
+	IsAlive() bool
 }
 
 type BasicPlayer struct { //basic player strcuct for attributes of a player / schema
@@ -12,4 +18,26 @@ type BasicPlayer struct { //basic player strcuct for attributes of a player / sc
 
 func NewBasicPlayer(health, strength, attack int) *BasicPlayer {
 	return &BasicPlayer{health, strength, attack}
+}
+
+//Method defination starts Here :=>
+
+func (p *BasicPlayer) GetHealth() int {
+	return p.health
+}
+
+func (p *BasicPlayer) GetStrength() int {
+	return p.strength
+}
+
+func (p *BasicPlayer) GetAttack() int {
+	return p.attack
+}
+
+func (p *BasicPlayer) ReduceHealth(amount int) int {
+	p.health -= amount
+}
+
+func (p *BasicPlayer) IsAlive() bool {
+	return p.health > 0
 }
